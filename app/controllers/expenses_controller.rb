a6c6f505208a250ed0d1b52f832b899e9a2b07e5
Matt <matthewmorgante@gmail.com>
2016-05-13 11:57:53 +0200

    ability to edit and destroy

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,11 +2,10 @@ class ExpensesController < ApplicationController
 before_action :find_expense, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    @expenses = Expense.all.order("created_at DESC")
   end 
 
   def show
-
   end 
 
   def new
@@ -28,11 +27,17 @@ before_action :find_expense, only: [:show, :edit, :update, :destroy]
   end
 
   def update
+    if @expense.update(expenses_params)
+      redirect_to @expense
+    else 
+      render "Edit"
+    end 
     
   end
 
   def destroy
-    
+    @expense.destroy
+    redirect_to root_path
   end
 
   private
